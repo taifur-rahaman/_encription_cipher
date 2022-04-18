1a47569ace122f8f921c1900508047fc01319c66
taifur-rahaman <taifur.rahaman@northsouth.edu>
2022-04-19 00:46:47 +0600

    v1.0.1

--- main.cpp
+++ main.cpp
@@ -29,6 +29,44 @@ int main(){
     cout << "Encryption Successful\n" << endl;
     cout << "Encrypted text : " << encrypt_text << endl;
 
-    // Decryption
-    // Code
+    int choice;
+    cout << "\nDo you want to decrypt the text?\n1. Yes\n2. No\nEnter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        // Decryption
+        cout << "\n\nDecrypted Text" << endl;
+
+        string decrypted_text;
+        for (size_t i{0}; i < key.length(); i++) {
+            for (char c: encrypt_text) {
+                if (key[i] == c) {
+                    decrypted_text.push_back(alphabet[i]);
+                }
+            }
+        }
+
+        // Rearranging index
+
+        string main_text;
+
+        for (size_t i{0}; i < user_text.length(); i++)
+        {
+            for (char c: decrypted_text)
+            {
+                if(user_text[i] == c)
+                {
+                    main_text.push_back(c);
+                    break;
+                }
+            }
+        }
+
+        cout << "\n\nThe encrypted text: " << encrypt_text << endl;
+        cout << "\n\nThe decrypted text: " << decrypted_text << endl;
+    }
+
+
+
+
 }
